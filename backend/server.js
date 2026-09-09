4235b025e2e8ebb708eddefefb530c67a8ddb071
balsoofi57-alt <balsoofi57@gmail.com>
2026-09-09 08:56:13 -0400

    Load pre-acceptance request privacy guard

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const database=require('./database');
 (async()=>{
   await database.initDurable();
   require('./response-security');
+  require('./request-privacy');
   require('./product-expansion');
   require('./server-mobile-payments');
 })().catch(e=>{console.error(JSON.stringify({event:'server_bootstrap_failed',message:e.message}));process.exit(1)});
