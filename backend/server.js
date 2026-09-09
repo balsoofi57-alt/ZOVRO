0fb9526f8e0eef0f5a1885958caf70dc3a268efb
balsoofi57-alt <balsoofi57@gmail.com>
2026-09-09 09:11:48 -0400

    Load consent enforcement and web consent injection

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const database=require('./database');
 (async()=>{
   await database.initDurable();
   require('./response-security');
+  require('./consent-guard');
   require('./request-privacy');
+  require('./html-inject');
   require('./product-expansion');
   require('./server-mobile-payments');
 })().catch(e=>{console.error(JSON.stringify({event:'server_bootstrap_failed',message:e.message}));process.exit(1)});
