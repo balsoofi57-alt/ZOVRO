0602ef8aa1918646d1878939c9b945748a8970d1
balsoofi57-alt <balsoofi57@gmail.com>
2026-09-22 14:15:21 -0400

    Bump PWA cache after mobile help repair

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='zovro-web-v4';
+const CACHE='zovro-web-v5';
 const CORE=['./','./index.html','./privacy.html','./terms.html','./support.html','./manifest.webmanifest','./assets/zovro-icon.svg','./website-final.css','./website-final.js'];
 
 self.addEventListener('install',event=>{
